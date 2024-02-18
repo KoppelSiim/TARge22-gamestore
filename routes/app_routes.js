c0aa7c0ce3f1134d107d277483cc37862f3e0fb1
Siim Koppel <siimk999@gmail.com>
2024-02-18 17:37:32 +0200

    remove influence controller and corresponding module.exports from app_routes.js

--- routes/app_routes.js
+++ routes/app_routes.js
@@ -1,5 +1,4 @@
 const gamesController = require('../controllers/GamesController.js')
-const influencersController = require('../controllers/InfluencersController.js')
 
 module.exports = (app) => {
     app.route("/games")
@@ -9,13 +8,5 @@ module.exports = (app) => {
         .get(gamesController.getById)    // get a game by id
         .put(gamesController.updateById) // change info of a game by id
         .delete(gamesController.deleteById) // delete a game by id
-
-    app.route("/influencers")
-        .get(influencersController.getAll)     // get all games as list
-    //    .post(gamesController.createNew) // create a new game
-    app.route("/influencers/:id")     
-        .get(influencersController.getById)    // get a game by id
-    //    .put(gamesController.updateById) // change info of a game by id
-    //    .delete(gamesController.deleteById) // delete a game by id
 
 }
