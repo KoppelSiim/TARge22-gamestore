b113ec7cef09ae5b481ef513c475730a157c76ee
Siim Koppel <siimk999@gmail.com>
2024-02-19 20:53:55 +0200

    Fix routing, api backend is working with these settings

--- db.js
+++ db.js
@@ -16,7 +16,6 @@ const db = {}
 db.Sequelize = Sequelize
 db.sequelize = sequelize
 db.games = require("./models/Game.model")(sequelize,Sequelize)
-db.influencers = require("./models/Influencer.model")(sequelize,Sequelize)
 
 
 async function Sync() {
--- routes/app_routes.js
+++ routes/app_routes.js
@@ -1,12 +1,6 @@
-const gamesController = require('../controllers/GamesController.js')
+const gamesController = require('../controllers/GamesController.js');
 
 module.exports = (app) => {
     app.route("/games")
-        .get(gamesController.getAll)     // get all games as list
-        .post(gamesController.createNew) // create a new game
-    app.route("/games/:id")     
-        .get(gamesController.getById)    // get a game by id
-        .put(gamesController.updateById) // change info of a game by id
-        .delete(gamesController.deleteById) // delete a game by id
-
-}
+        .get(gamesController.getAll);  // get all games as list
+};
