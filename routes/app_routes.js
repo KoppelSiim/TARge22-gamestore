8d986cab79612df8c96f69809af6cf10be362b9d
Siim Koppel <siimk999@gmail.com>
2024-02-23 10:01:13 +0200

    Add welcome message to '/ ' path to my gamestore

--- routes/app_routes.js
+++ routes/app_routes.js
@@ -1,6 +1,12 @@
 const gamesController = require('../controllers/GamesController.js');
 
 module.exports = (app) => {
+    // Welcome message to my gamestore
+    app.get("/", (req, res) => {
+        res.send("Welcome to my games tore!");
+    });
     app.route("/games")
         .get(gamesController.getAll);  // get all games as list
+    /*app.route("/games/:id")     
+        .get(gamesController.getById)    // get a game by id*/
 };
