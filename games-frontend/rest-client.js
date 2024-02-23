bd1ea88319e51bf5aa11a189679f477b2f17c65c
Siim Koppel <siimk999@gmail.com>
2024-02-23 11:06:52 +0200

    Front end package changes

--- games-frontend/rest-client.js
+++ games-frontend/rest-client.js
@@ -4,13 +4,10 @@ const vue = Vue.createApp({
         return { 
             games: [],
             gameInModal: {name: null}, 
-            //influencers: [],
-            //influencerInModal: {onlinename: null},
         }
 },
 async created() {
     this.games = await (await fetch('http://localhost:8080/games')).json();
-    //this.influencers = await (await fetch('http://localhost:8080/influencers')).json();
 },
 methods: {
     getGame: async function(id) {
@@ -18,9 +15,5 @@ methods: {
         let gameInfoModal = new bootstrap.Modal(document.getElementById('gameInfoModal'), {})
         gameInfoModal.show();
     },
-/*     getInfluencer: async function(id) {
-        this.influencerInModal = await (await fetch(`http://localhost:8080/games/${id}`)).json();
-        let influencerInModal = new bootstrap.Modal(document.getElementById ('influencerInfoModal'), {})
-    } */
 }
 }).mount('#app')
