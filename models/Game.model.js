d97a81f14e432f0a7b1e4b080d2dda448f34301d
Siim Koppel <siimk999@gmail.com>
2024-03-02 15:00:05 +0200

    Implement [#187121762]

--- models/Game.model.js
+++ models/Game.model.js
@@ -1,5 +1,12 @@
 module.exports = (sequelize, Sequelize) => {
     const Game = sequelize.define("Games", {
+        userId: {
+            type: Sequelize.INTEGER,
+            references: {
+                model: 'Users', 
+                key: 'userId'  
+            }
+        },
         gameId: {
             type: Sequelize.INTEGER,
             primaryKey: true,
