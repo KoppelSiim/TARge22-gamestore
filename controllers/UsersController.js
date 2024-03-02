190b4875e4182d1addfb3dcd7829676997f0e45f
Siim Koppel <siimk999@gmail.com>
2024-03-02 18:56:37 +0200

    Get a list of all User id-s [#187156291]

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,7 +2,16 @@ const { db } = require('../db');
 db.sequelize.sync();
 const User = db.users;
 
-
+// Get list of user Id-s
+exports.getAll = async (req, res) => {
+    try {
+        const users = await User.findAll({ attributes: ["userId"] });
+        res.status(200).json(users);
+    } catch (error) {
+        console.error('users:', error);
+        res.status(500).json({ error: 'cannot find  userid' });
+    }
+};
 
 // Get base URL
 getBaseUrl = (request) => {
