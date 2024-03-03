bfd46e945c4eaf12ede2209467cfa4c3bb8f43db
Siim Koppel <siimk999@gmail.com>
2024-03-03 16:16:59 +0200

    Add purchaseController buyGame function

--- controllers/PurchaseController.js
+++ controllers/PurchaseController.js
@@ -1,8 +1,36 @@
 const { db } = require('../db');
-db.Sequelize.sync();
-const { Purchase, User, Game } = require('../db');
+db.sequelize.sync();
+const { Game, User, Purchase } = require('../db');
 
+exports.buyGame = async (req, res) => {
+    try {
+        // get the gameId
+        const { gameId } = req.params; 
+        // find the game
+        const game = await Game.findByPk(gameId);
+        // game not found
+        if (!game) {
+            return res.status(404).json({ success: false, error: 'Game not found' });
+        }
+        // get the userId
+        const{userId} = req.body;
+        const user = await User.findByPk(userId);
+        if(!user){
+            return res.status(404).json({success:false, error: 'User not found'});
+        }
 
+        // Buy the game
+        const purchase = await Purchase.create({
+            userId: req.user.id, 
+            gameId,
+        });
+
+        return res.status(201).json({ success: true, purchase });
+    } catch (error) {
+        console.error('Error buying game:', error);
+        res.status(500).json({ success: false, error: 'Error creating the purchase in the database' });
+    }
+};
 
 // Get base URL
 getBaseUrl = (request) => {
