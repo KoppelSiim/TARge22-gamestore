d77319e6180ea5df6215e395bbedd884f5e72c61
Siim Koppel <siimk999@gmail.com>
2024-03-03 19:36:22 +0200

    Implement buy game feature [#187157794]

--- controllers/PurchaseController.js
+++ controllers/PurchaseController.js
@@ -1,41 +1,31 @@
-const { db } = require('../db');
-db.sequelize.sync();
-const { Game, User, Purchase } = require('../db');
+const { db, Sync } = require('../db');
+const Game = db.games;
+const User = db.users;
+const Purchase = db.purchases;
 
+// Buy game feature
 exports.buyGame = async (req, res) => {
     try {
-        // get the gameId
-        const { gameId } = req.params; 
-        // find the game
+        const { gameId, userId } = req.body;
         const game = await Game.findByPk(gameId);
-        // game not found
+
         if (!game) {
             return res.status(404).json({ success: false, error: 'Game not found' });
         }
-        // get the userId
-        const{userId} = req.body;
+
         const user = await User.findByPk(userId);
-        if(!user){
-            return res.status(404).json({success:false, error: 'User not found'});
+
+        if (!user) {
+            return res.status(404).json({ success: false, error: 'User not found' });
         }
 
-        // Buy the game
         const purchase = await Purchase.create({
-            userId: req.user.id, 
+            userId: user.id,
             gameId,
         });
-
         return res.status(201).json({ success: true, purchase });
     } catch (error) {
         console.error('Error buying game:', error);
         res.status(500).json({ success: false, error: 'Error creating the purchase in the database' });
     }
-};
-
-// Get base URL
-getBaseUrl = (request) => {
-    return (
-        (request.connection && request.connection.encrypted ? 'https' : 'http') +
-        `://${request.headers.host}`
-    );
 };
