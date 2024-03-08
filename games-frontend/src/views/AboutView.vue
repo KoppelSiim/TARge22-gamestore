533f79a1558a11d2c391b72187475d3d24f03046
Siim Koppel <siimk999@gmail.com>
2024-03-08 16:20:53 +0200

    Add some style to about page

--- games-frontend/src/views/AboutView.vue
+++ games-frontend/src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Cool gamestore</h1>
+    <h2 id="about">About my gamestore</h2>
   </div>
 </template>
 
@@ -10,6 +11,14 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+  }
+  h1 {
+    color: #4f19b4;
+    margin-bottom: 10px;
+    padding: 20px;
+  }
+  #about{
+    color:rgb(74, 109, 79);
   }
 }
 </style>
