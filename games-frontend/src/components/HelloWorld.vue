0938739408969a22b7d2f16fed3bdfebd6e44731
Siim Koppel <siimk999@gmail.com>
2024-03-08 17:01:49 +0200

    Add title to HelloWorld view

--- games-frontend/src/components/HelloWorld.vue
+++ games-frontend/src/components/HelloWorld.vue
@@ -8,19 +8,18 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      Hello front end with vue and vite
-      Hello front end with vue and vite
-      Hello front end with vue and vite
-      Hello front end with vue and vite
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
+    <h1>
+      Welcome to my game store
+    </h1>
   </div>
 </template>
 
 <style scoped>
 h1 {
-  font-weight: 500;
+  font-weight: 100;
   font-size: 2.6rem;
   position: relative;
   top: -10px;
