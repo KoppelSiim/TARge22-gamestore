fddbe08c220ba1a212ba8f95757fa5b643b0fe76
Siim Koppel <siimk999@gmail.com>
2024-03-16 12:11:30 +0200

    Update FrontPage reference name to Front

--- games-frontend/src/App.vue
+++ games-frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/FrontPage.vue'
+import Front from './components/FrontPage.vue'
 </script>
 
 <template>
@@ -8,8 +8,7 @@ import HelloWorld from './components/FrontPage.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+      <Front msg="You did it!" />
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
